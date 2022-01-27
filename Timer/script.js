a77b94bf8ff9be179e4c748da1e02d7c7d3b95d7
ercinayse <ercinayse97@gmail.com>
2022-01-27 23:48:42 +0300

    countdown to my birthday

---
+++ Timer/script.js
@@ -0,0 +1,21 @@
+
+function countDown() {
+  const today = new Date().getTime();
+  const birthday = "26 September 2022";
+  const myBirthdayDate = new Date(birthday).getTime();
+  let diff = (myBirthdayDate - today) / 1000;
+
+  const remainDays = Math.floor(diff / (3600 * 24));
+  const remainHours = Math.floor((diff / 3600) % 24);
+  const remainMinutes = Math.floor((diff / 60) % 60);
+  const remainSeconds = Math.floor(diff % 60);
+
+  document.getElementById("days").innerHTML = remainDays;
+  document.getElementById("hours").innerHTML = remainHours;
+  document.getElementById("minutes").innerHTML = remainMinutes;
+  document.getElementById("seconds").innerHTML = remainSeconds;
+}
+
+countDown();
+
+setInterval(countDown,1000);
